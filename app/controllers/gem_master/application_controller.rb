ea7d85d3609b2be78066030abcb5c8f1563bd071
Jiří Čech <jiricech94@gmail.com>
2019-02-20 22:11:26 +0100

    Fix application controller dependency and add top level comment

--- app/controllers/gem_master/application_controller.rb
+++ app/controllers/gem_master/application_controller.rb
@@ -1,6 +1,7 @@
-require_dependency "blorgh/application_controller"
+require_dependency 'gem_master/application_controller'
 
 module GemMaster
+  # Engine controller
   class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
   end
